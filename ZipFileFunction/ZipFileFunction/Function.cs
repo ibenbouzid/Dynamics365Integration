0cea470c5efe4b6e3f1cac995ec3d48a4381d9e4
imad <imad_benb@yahoo.fr>
2019-11-30 19:21:10 +0100

    0

--- ZipFileFunction/ZipFileFunction/Function.cs
+++ ZipFileFunction/ZipFileFunction/Function.cs
@@ -29,8 +29,7 @@ namespace ZipFileFunction
             dynamic body = await req.Content.ReadAsAsync<object>();
 
             var dataStream = new MemoryStream();
-            var cloudStorageAccount = new CloudStorageAccount(new StorageCredentials("ukieb", "nRabLGIiS5b3O8XPf8F0AI91uzNN4zxzDJePhDu1tXf6hH3iGYX1M1gR2AEhp1p9jUo8Tmf+ik2SjAHc7E89fw=="), true);
-            //var container = cloudStorageAccount.CreateCloudBlobClient().GetContainerReference("assetphotos");
+            var cloudStorageAccount = new CloudStorageAccount(new StorageCredentials("storage account", "key"), true);
             var container = cloudStorageAccount.CreateCloudBlobClient().GetContainerReference(body.container.ToString());
             var blobFileNames = JsonConvert.DeserializeObject <string[]> (body.files.ToString());
 
@@ -49,9 +48,31 @@ namespace ZipFileFunction
                 zipOutputStream.Close();
             }
             byte[] zipBytes = dataStream.ToArray();
-            string base64String = Convert.ToBase64String(zipBytes);
+            //string base64String = Convert.ToBase64String(zipBytes);
+            string result = System.Text.Encoding.UTF8.GetString(zipBytes);
+            //StreamReader reader = new StreamReader(dataStream);
+            //string zipBytes = reader.ReadToEnd();
 
-            return req.CreateResponse(HttpStatusCode.OK, new { ZipFilename = "zipedfile.zip", ZipFile = zipBytes });
+
+            /*var memoryStream = new MemoryStream();
+            using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    var demoFile = archive.CreateEntry("foo.txt");
+
+                    using (var entryStream = demoFile.Open())
+                    using (var streamWriter = new StreamWriter(entryStream))
+                    {
+                        streamWriter.Write("Bar!");
+                    }
+                }
+
+                using (var fileStream = new FileStream(@"C:\Temp\test.zip", FileMode.Create))
+                {
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    memoryStream.CopyTo(fileStream);
+                }*/
+
+            return req.CreateResponse(HttpStatusCode.OK, new { ZipFilename = "zipedfile.zip", ZipFile = zipBytes, datastream = dataStream, result = result });
         }
     }
 }
